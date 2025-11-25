7de6cd61830da62d40129db0341c13f92194fc5e
David Brett <davidbrett2002@gmail.com>
2025-11-25 12:41:41 +0100

    Remove Type From QueryItem and Remove QueryType Enum

--- Flow.Launcher.Plugin.CustomGroups/QueryItem.cs
+++ Flow.Launcher.Plugin.CustomGroups/QueryItem.cs
@@ -6,14 +6,9 @@ namespace Flow.Launcher.Plugin.CustomGroups
     public class QueryItem : BaseModel
     {
         
-
-        // public static Array QueryTypeValues => Enum.GetValues(typeof(QueryType));
-
         public string Name { get; set; }
 
         public string Query { get; set; }
-
-        public QueryType Type { get; set; }
 
     }
 }
