7831b24d0ec1c54bf9914a7e33a83f71915df9ad
David Brett <davidbrett2002@gmail.com>
2026-01-24 12:27:36 +0100

    Ensure all query items have a name value by returning Query if name is null or empty

--- Flow.Launcher.Plugin.QueryGroups/QueryItem.cs
+++ Flow.Launcher.Plugin.QueryGroups/QueryItem.cs
@@ -5,8 +5,15 @@ namespace Flow.Launcher.Plugin.QueryGroups
     
     public class QueryItem : BaseModel
     {
-        
-        public string Name { get; set; }
+        private string _name;
+
+        public string Name {
+            set => _name = value;
+            get {
+                    if (string.IsNullOrEmpty(_name)) return Query;
+                    else return _name;
+                }
+        }
 
         public string Query { get; set; }
 
