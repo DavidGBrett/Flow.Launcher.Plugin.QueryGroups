3985e2d81b3df76bb8b67a7ec4ca630e80081683
David Brett <davidbrett2002@gmail.com>
2026-02-13 19:25:03 +0100

    Make add group query give error for invalid names rather than ignoring invalid part

--- Flow.Launcher.Plugin.QueryGroups/PluginQuerySyntax/Definitions/AddGroupCommandDefinition.cs
+++ Flow.Launcher.Plugin.QueryGroups/PluginQuerySyntax/Definitions/AddGroupCommandDefinition.cs
@@ -1,6 +1,7 @@
 
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Linq;
 using Flow.Launcher.Plugin.QueryGroups;
 
 namespace Flow.Launcher.Plugin.QueryGroups.PluginQuerySyntax
@@ -22,8 +23,16 @@ namespace Flow.Launcher.Plugin.QueryGroups.PluginQuerySyntax
 
         public string ParseQuery(QueryPartsInfo queryPartsInfo)
         {
+            // item query is everything after the first separator
+            // this will catch separators that we dont want, but its important to show an error than silently ignore
+            string groupName = "";
+            if (queryPartsInfo.Parts.Count > 1)
+            {
+                groupName = string.Join(PluginConstants.QuerySeparator, queryPartsInfo.Parts.Skip(1));
+            }
+
             return (
-                queryPartsInfo.Parts.Count > 1 ? queryPartsInfo.Parts[1] : ""
+                groupName
             );
         }
 
