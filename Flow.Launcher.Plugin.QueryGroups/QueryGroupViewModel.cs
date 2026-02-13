e386803645db4ebd0073de1a6c5d0368a3c8f2c8
David Brett <davidbrett2002@gmail.com>
2026-02-13 16:44:14 +0100

    Allow valid edits to group name and show invalid warning on settings page

--- Flow.Launcher.Plugin.QueryGroups/QueryGroupViewModel.cs
+++ Flow.Launcher.Plugin.QueryGroups/QueryGroupViewModel.cs
@@ -8,13 +8,49 @@ namespace Flow.Launcher.Plugin.QueryGroups
 
         public ObservableCollection<QueryItem> QueryItems {get; set;}
 
-        public string EditName {get; set;}
+        private SettingsViewModel _settings;
+
+        private string _editName;
+
+        private bool _isEditNameInvalid = false;
+        public bool isEditNameInvalid {
+            get { return _isEditNameInvalid;} 
+            private set
+            {
+                _isEditNameInvalid = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string EditName {
+            get {return _editName;}
+            set
+            {
+                _editName = value;
+
+                if (_editName == QueryGroup.Name)
+                {
+                    isEditNameInvalid = false;
+                }
+                
+                else if (_settings.Settings.isNewGroupNameValid(EditName))
+                {
+                    QueryGroup.Name = _editName;
+                    isEditNameInvalid = false;
+                }
+                else
+                {
+                    isEditNameInvalid = true;
+                }
+            }
+        }
 
          public QueryGroupViewModel(
             QueryGroup queryGroup,
             SettingsViewModel settings
         )
         {
+            _settings = settings;
             QueryGroup = queryGroup;
             EditName = queryGroup.Name;
             QueryItems = queryGroup.QueryItems;
