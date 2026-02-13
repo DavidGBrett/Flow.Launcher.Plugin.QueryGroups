3c15835593f05fbe73e6c6283a7a2a7e82b8b574
David Brett <davidbrett2002@gmail.com>
2026-02-14 00:20:04 +0100

    Update QueryGroupVMs when QueryGroups CollectionChanged Now changes made to groups in the search interface will show up on the settings page instantly

--- Flow.Launcher.Plugin.QueryGroups/SettingsViewModel.cs
+++ Flow.Launcher.Plugin.QueryGroups/SettingsViewModel.cs
@@ -1,4 +1,5 @@
 using System.Collections.ObjectModel;
+using System.Collections.Specialized;
 using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Windows.Input;
@@ -35,20 +36,71 @@ namespace Flow.Launcher.Plugin.QueryGroups
             AddGroupCommand = new RelayCommand(AddGroup);
             DeleteGroupCommand = new RelayCommand<QueryGroupViewModel>(DeleteGroup);
 
+            Settings.QueryGroups.CollectionChanged += OnQueryGroupsChanged;
         }
 
         public Settings Settings { get; }
 
         private void AddGroup()
         {
-            var newGroup = Settings.AddGroup();
-            QueryGroupVMs.Add(new QueryGroupViewModel(newGroup,this));
+            Settings.AddGroup();
         }
 
         private void DeleteGroup(QueryGroupViewModel groupVM)
         {
             Settings.QueryGroups.Remove(groupVM.QueryGroup);
-            QueryGroupVMs.Remove(groupVM);
+        }
+
+        private void OnQueryGroupsChanged(object sender, NotifyCollectionChangedEventArgs e)
+        {
+            switch (e.Action)
+            {
+                case NotifyCollectionChangedAction.Add:
+                    if (e.NewItems == null || Settings.QueryGroups.Count == QueryGroupVMs.Count)
+                    {
+                        return;
+                    }
+
+                    foreach (QueryGroup newGroup in e.NewItems)
+                    {
+                        QueryGroupVMs.Add(new QueryGroupViewModel(newGroup,this));
+                    }
+                    break;
+
+                case NotifyCollectionChangedAction.Remove:
+                    if (e.OldItems == null || Settings.QueryGroups.Count == QueryGroupVMs.Count)
+                    {
+                        return;
+                    }
+
+                    foreach (QueryGroup oldGroup in e.OldItems)
+                    {
+                        QueryGroupViewModel toRemove = QueryGroupVMs.FirstOrDefault((qg)=> qg.QueryGroup.Name == oldGroup.Name);
+                        QueryGroupVMs.Remove(toRemove);
+                    }
+                    break;
+
+                case NotifyCollectionChangedAction.Replace:
+                    foreach (QueryGroup oldGroup in e.OldItems)
+                    {
+                        QueryGroupViewModel toRemove = QueryGroupVMs.FirstOrDefault((qg)=> qg.QueryGroup.Name == oldGroup.Name);
+                        QueryGroupVMs.Remove(toRemove);
+                    }
+                    foreach (QueryGroup newGroup in e.NewItems)
+                    {
+                        QueryGroupVMs.Add(new QueryGroupViewModel(newGroup,this));
+                    }
+                    break;
+
+                case NotifyCollectionChangedAction.Move:
+                    break;
+
+                case NotifyCollectionChangedAction.Reset:
+                    QueryGroupVMs = new ObservableCollection<QueryGroupViewModel>(
+                        Settings.QueryGroups
+                            .Select(g => new QueryGroupViewModel(g, this)));
+                    break;
+            }
         }
     }
 }
